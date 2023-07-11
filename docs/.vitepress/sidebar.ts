aafb12430adf91f078859dfbeced0fa8744360f7
windy <1519105541@qq.com>
2023-07-11 16:42:26 +0800

    fix: 添加目录

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -114,4 +114,67 @@ export default [
       { text: "其他-未分类 4", link: "/others/other2" },
     ],
   },
+  {
+    text: "浏览器工作原理与实践",
+    items: [
+      {
+        text: "开篇词 (1讲)",
+        items: [{ text: "开篇词", link: "/browser/开篇词 (1讲)/开篇词" }],
+      },
+      {
+        text: "宏观视角下的浏览器 (6讲)",
+        items: [
+          {
+            text: "01 Chrome架构",
+            link: "/browser/宏观视角下的浏览器 (6讲)/01 | Chrome架构：仅仅打开了1个页面，为什么有4个进程？",
+          },
+          {
+            text: "02 TCP协议",
+            link: "/browser/宏观视角下的浏览器 (6讲)/02 | TCP协议：如何保证页面文件能被完整送达浏览器？",
+          },
+          {
+            text: "03 HTTP请求流程",
+            link: "/browser/宏观视角下的浏览器 (6讲)/03 | HTTP请求流程：为什么很多站点第二次打开速度会很快？",
+          },
+          {
+            text: "04 导航流程",
+            link: "/browser/宏观视角下的浏览器 (6讲)/04 | 导航流程：从输入URL到页面展示，这中间发生了什么？",
+          },
+          {
+            text: "05 渲染流程（上）",
+            link: "/browser/宏观视角下的浏览器 (6讲)/05 | 渲染流程（上）：HTML、CSS和JavaScript，是如何变成页面的？",
+          },
+          {
+            text: "06 渲染流程（下）",
+            link: "/browser/宏观视角下的浏览器 (6讲)/06 | 渲染流程（下）：HTML、CSS和JavaScript，是如何变成页面的？",
+          },
+        ],
+      },
+      {
+        text: "浏览器中的JavaScript执行机制 (5讲)",
+        items: [
+          {
+            text: "07 变量提升",
+            link: "/browser/浏览器中的JavaScript执行机制 (5讲)/07 | 变量提升：JavaScript代码是按顺序执行的吗？",
+          },
+          {
+            text: "08 调用栈",
+            link: "/browser/浏览器中的JavaScript执行机制 (5讲)/08 | 调用栈：为什么JavaScript代码会出现栈溢出？",
+          },
+          {
+            text: "09 块级作用域",
+            link: "/browser/浏览器中的JavaScript执行机制 (5讲)/09 | 块级作用域：var缺陷以及为什么要引入let和const？",
+          },
+          {
+            text: "10 作用域链和闭包",
+            link: "/browser/浏览器中的JavaScript执行机制 (5讲)/10 | 作用域链和闭包：代码中出现相同的变量，JavaScript引擎是如何选择的？",
+          },
+          {
+            text: "11 this",
+            link: "/browser/浏览器中的JavaScript执行机制 (5讲)/11 | this：从JavaScript执行上下文的视角讲清楚this",
+          },
+        ],
+      },
+    ],
+  },
 ];
