9d6a3ea871e78aec3c737ad8b8483f7c8d8b2079
joy <1519105541@qq.com>
2024-03-13 01:03:49 +0800

    feat: 面试题

---
+++ docs/algorithm/test.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var kSum = function(nums, k) {
+    
+};
+
+const word = '86217457695827338571';
+const m = 8;
+
+const res = kSum(word, m);
+console.log(res)
