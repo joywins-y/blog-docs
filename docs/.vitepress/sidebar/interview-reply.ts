45982fdb19bb6aa29cd431c9b6c65fbb2944cf7f
joy <1519105541@qq.com>
2024-04-24 14:25:53 +0800

    feat: 新增

--- docs/.vitepress/sidebar/interview-reply.ts
+++ docs/.vitepress/sidebar/interview-reply.ts
@@ -1,261 +1,265 @@
 export default [
-    {
-        text: '面试真题汇总',
-        items: [
-            {
-                text: '汇总',
-                link: '/面试复盘/汇总',
-            },
-            {
-                text: '面试题库',
-                link: '/面试复盘/面试题库',
-            },
-            {
-                text: '哲玄直播面试题',
-                link: '/面试复盘/哲玄直播面试题',
-            },
-        ],
-    },
-    {
-        text: '面试真题',
-        items: [
-            {
-                text: '2024年3月4号',
-                link: '/面试复盘/2024年3月4号',
-            },
-            {
-                text: '2024年3月6号',
-                link: '/面试复盘/2024年3月6号',
-            },
-            {
-                text: '2024年3月7号',
-                link: '/面试复盘/2024年3月7号',
-            },
-            {
-                text: '2024年3月13号',
-                link: '/面试复盘/2024年3月13号',
-            },
-            {
-                text: '2024年3月28号',
-                link: '/面试复盘/2024年3月28号',
-            },
-            {
-                text: '2024年4月1号',
-                link: '/面试复盘/2024年4月1号',
-            },
-            {
-                text: '数字马力面试题',
-                link: '/面试复盘/数字马力面试题',
-            },
-            {
-                text: '前端面试宝典',
-                link: '/面试复盘/前端面试宝典',
-            },
-            {
-                text: '第一版面试题',
-                link: '/面试复盘/第一版面试题',
-            },
-            {
-                text: '大师课面试题',
-                link: '/面试复盘/大师课面试题',
-            },
-        ],
-    },
-    {
-        text: 'React 面试题',
-        items: [
-            {
-                text: 'React 面试题汇总',
-                link: '/面试复盘/React/React面试题汇总',
-            },
-            {
-                text: '前端框架的理解',
-                link: '/面试复盘/React/前端框架的理解',
-            },
-            {
-                text: '对 React 的理解',
-                link: '/面试复盘/React/对React的理解',
-            },
-            {
-                text: 'React 和 Vue 描述页面的区别',
-                link: '/面试复盘/React/React 和 Vue 描述页面的区别',
-            },
-            {
-                text: '前端框架的分类',
-                link: '/面试复盘/React/前端框架的分类',
-            },
-            {
-                text: '虚拟 DOM',
-                link: '/面试复盘/React/虚拟 DOM',
-            },
-            {
-                text: 'React 整体架构',
-                link: '/面试复盘/React/React 整体架构',
-            },
-            {
-                text: 'React 渲染流程',
-                link: '/面试复盘/React/React 渲染流程',
-            },
-            {
-                text: 'Fiber双缓冲',
-                link: '/面试复盘/React/Fiber双缓冲',
-            },
-            {
-                text: 'MessageChannel',
-                link: '/面试复盘/React/MessageChannel',
-            },
-            {
-                text: 'Scheduler调度普通任务',
-                link: '/面试复盘/React/Scheduler调度普通任务',
-            },
-            {
-                text: 'Scheduler调度延时任务',
-                link: '/面试复盘/React/Scheduler调度延时任务',
-            },
-            {
-                text: '最小堆',
-                link: '/面试复盘/React/最小堆',
-            },
-            {
-                text: 'React中的位运算',
-                link: '/面试复盘/React/React中的位运算',
-            },
-            {
-                text: 'beginWork工作流程',
-                link: '/面试复盘/React/beginWork工作流程',
-            },
-            {
-                text: 'completeWork工作流程',
-                link: '/面试复盘/React/completeWork工作流程',
-            },
-            {
-                text: '图解diff算法',
-                link: '/面试复盘/React/图解diff算法',
-            },
-            {
-                text: 'commit 工作流程',
-                link: '/面试复盘/React/commit 工作流程',
-            },
-            {
-                text: 'lane模型',
-                link: '/面试复盘/React/lane模型',
-            },
-            {
-                text: 'React 中的事件',
-                link: '/面试复盘/React/React 中的事件',
-            },
-            {
-                text: 'Hooks原理',
-                link: '/面试复盘/React/Hooks原理',
-            },
-            {
-                text: 'useState和useReducer',
-                link: '/面试复盘/React/useState和useReducer',
-            },
-            {
-                text: 'effect相关hook',
-                link: '/面试复盘/React/effect相关hook',
-            },
-            {
-                text: 'useCallback和useMemo',
-                link: '/面试复盘/React/useCallback和useMemo',
-            },
-            {
-                text: 'useRef',
-                link: '/面试复盘/React/useRef',
-            },
-            {
-                text: 'Update',
-                link: '/面试复盘/React/Update',
-            },
-            {
-                text: '性能优化策略之eagerState',
-                link: '/面试复盘/React/性能优化策略之eagerState',
-            },
-            {
-                text: '性能优化策略之bailout',
-                link: '/面试复盘/React/性能优化策略之bailout',
-            },
-            {
-                text: 'bailout和ContextAPI',
-                link: '/面试复盘/React/bailout和ContextAPI',
-            },
-            {
-                text: '性能优化对日常开发启示',
-                link: '/面试复盘/React/性能优化对日常开发启示',
-            },
-        ],
-    },
-    {
-        text: 'HTML',
-        items: [
-            {
-                text: 'HTML 面试题汇总',
-                link: '/面试复盘/HTML/HTML 面试题汇总',
-            },
-        ],
-    },
-    {
-        text: 'CSS',
-        items: [
-            {
-                text: 'CSS 面试题汇总',
-                link: '/面试复盘/CSS/CSS 面试题汇总',
-            },
-        ],
-    },
-    {
-        text: 'JavaScript',
-        items: [
-            {
-                text: 'JavaScript 面试题汇总',
-                link: '/面试复盘/JavaScript/JavaScript 面试题汇总',
-            },
-        ],
-    },
-    {
-        text: '网络',
-        items: [
-            {
-                text: 'HTML 面试题汇总',
-                link: '/面试复盘/网络/网络面试题汇总',
-            },
-        ],
-    },
-    {
-        text: '工程化',
-        items: [
-            {
-                text: '工程化面试题汇总',
-                link: '/面试复盘/工程化/工程化面试题汇总',
-            },
-        ],
-    },
-    {
-        text: '浏览器',
-        items: [
-            {
-                text: '浏览器面试题汇总',
-                link: '/面试复盘/浏览器/浏览器面试题汇总',
-            },
-        ],
-    },
-    {
-        text: 'Promise',
-        items: [
-            {
-                text: 'Promise 面试题汇总',
-                link: '/面试复盘/Promise/Promise面试题归总',
-            },
-        ],
-    },
-    {
-        text: 'Vue',
-        items: [
-            {
-                text: 'Vue 笔面试题汇总',
-                link: '/面试复盘/Vue/vue笔面试题汇总',
-            },
-        ],
-    },
+  {
+    text: "面试真题汇总",
+    items: [
+      {
+        text: "汇总",
+        link: "/面试复盘/汇总",
+      },
+      {
+        text: "面试题库",
+        link: "/面试复盘/面试题库",
+      },
+      {
+        text: "哲玄直播面试题",
+        link: "/面试复盘/哲玄直播面试题",
+      },
+    ],
+  },
+  {
+    text: "面试真题",
+    items: [
+      {
+        text: "2024年3月4号",
+        link: "/面试复盘/2024年3月4号",
+      },
+      {
+        text: "2024年3月6号",
+        link: "/面试复盘/2024年3月6号",
+      },
+      {
+        text: "2024年3月7号",
+        link: "/面试复盘/2024年3月7号",
+      },
+      {
+        text: "2024年3月13号",
+        link: "/面试复盘/2024年3月13号",
+      },
+      {
+        text: "2024年3月28号",
+        link: "/面试复盘/2024年3月28号",
+      },
+      {
+        text: "2024年4月1号",
+        link: "/面试复盘/2024年4月1号",
+      },
+      {
+        text: "2024年4月24号",
+        link: "/面试复盘/2024年4月24号",
+      },
+      {
+        text: "数字马力面试题",
+        link: "/面试复盘/数字马力面试题",
+      },
+      {
+        text: "前端面试宝典",
+        link: "/面试复盘/前端面试宝典",
+      },
+      {
+        text: "第一版面试题",
+        link: "/面试复盘/第一版面试题",
+      },
+      {
+        text: "大师课面试题",
+        link: "/面试复盘/大师课面试题",
+      },
+    ],
+  },
+  {
+    text: "React 面试题",
+    items: [
+      {
+        text: "React 面试题汇总",
+        link: "/面试复盘/React/React面试题汇总",
+      },
+      {
+        text: "前端框架的理解",
+        link: "/面试复盘/React/前端框架的理解",
+      },
+      {
+        text: "对 React 的理解",
+        link: "/面试复盘/React/对React的理解",
+      },
+      {
+        text: "React 和 Vue 描述页面的区别",
+        link: "/面试复盘/React/React 和 Vue 描述页面的区别",
+      },
+      {
+        text: "前端框架的分类",
+        link: "/面试复盘/React/前端框架的分类",
+      },
+      {
+        text: "虚拟 DOM",
+        link: "/面试复盘/React/虚拟 DOM",
+      },
+      {
+        text: "React 整体架构",
+        link: "/面试复盘/React/React 整体架构",
+      },
+      {
+        text: "React 渲染流程",
+        link: "/面试复盘/React/React 渲染流程",
+      },
+      {
+        text: "Fiber双缓冲",
+        link: "/面试复盘/React/Fiber双缓冲",
+      },
+      {
+        text: "MessageChannel",
+        link: "/面试复盘/React/MessageChannel",
+      },
+      {
+        text: "Scheduler调度普通任务",
+        link: "/面试复盘/React/Scheduler调度普通任务",
+      },
+      {
+        text: "Scheduler调度延时任务",
+        link: "/面试复盘/React/Scheduler调度延时任务",
+      },
+      {
+        text: "最小堆",
+        link: "/面试复盘/React/最小堆",
+      },
+      {
+        text: "React中的位运算",
+        link: "/面试复盘/React/React中的位运算",
+      },
+      {
+        text: "beginWork工作流程",
+        link: "/面试复盘/React/beginWork工作流程",
+      },
+      {
+        text: "completeWork工作流程",
+        link: "/面试复盘/React/completeWork工作流程",
+      },
+      {
+        text: "图解diff算法",
+        link: "/面试复盘/React/图解diff算法",
+      },
+      {
+        text: "commit 工作流程",
+        link: "/面试复盘/React/commit 工作流程",
+      },
+      {
+        text: "lane模型",
+        link: "/面试复盘/React/lane模型",
+      },
+      {
+        text: "React 中的事件",
+        link: "/面试复盘/React/React 中的事件",
+      },
+      {
+        text: "Hooks原理",
+        link: "/面试复盘/React/Hooks原理",
+      },
+      {
+        text: "useState和useReducer",
+        link: "/面试复盘/React/useState和useReducer",
+      },
+      {
+        text: "effect相关hook",
+        link: "/面试复盘/React/effect相关hook",
+      },
+      {
+        text: "useCallback和useMemo",
+        link: "/面试复盘/React/useCallback和useMemo",
+      },
+      {
+        text: "useRef",
+        link: "/面试复盘/React/useRef",
+      },
+      {
+        text: "Update",
+        link: "/面试复盘/React/Update",
+      },
+      {
+        text: "性能优化策略之eagerState",
+        link: "/面试复盘/React/性能优化策略之eagerState",
+      },
+      {
+        text: "性能优化策略之bailout",
+        link: "/面试复盘/React/性能优化策略之bailout",
+      },
+      {
+        text: "bailout和ContextAPI",
+        link: "/面试复盘/React/bailout和ContextAPI",
+      },
+      {
+        text: "性能优化对日常开发启示",
+        link: "/面试复盘/React/性能优化对日常开发启示",
+      },
+    ],
+  },
+  {
+    text: "HTML",
+    items: [
+      {
+        text: "HTML 面试题汇总",
+        link: "/面试复盘/HTML/HTML 面试题汇总",
+      },
+    ],
+  },
+  {
+    text: "CSS",
+    items: [
+      {
+        text: "CSS 面试题汇总",
+        link: "/面试复盘/CSS/CSS 面试题汇总",
+      },
+    ],
+  },
+  {
+    text: "JavaScript",
+    items: [
+      {
+        text: "JavaScript 面试题汇总",
+        link: "/面试复盘/JavaScript/JavaScript 面试题汇总",
+      },
+    ],
+  },
+  {
+    text: "网络",
+    items: [
+      {
+        text: "HTML 面试题汇总",
+        link: "/面试复盘/网络/网络面试题汇总",
+      },
+    ],
+  },
+  {
+    text: "工程化",
+    items: [
+      {
+        text: "工程化面试题汇总",
+        link: "/面试复盘/工程化/工程化面试题汇总",
+      },
+    ],
+  },
+  {
+    text: "浏览器",
+    items: [
+      {
+        text: "浏览器面试题汇总",
+        link: "/面试复盘/浏览器/浏览器面试题汇总",
+      },
+    ],
+  },
+  {
+    text: "Promise",
+    items: [
+      {
+        text: "Promise 面试题汇总",
+        link: "/面试复盘/Promise/Promise面试题归总",
+      },
+    ],
+  },
+  {
+    text: "Vue",
+    items: [
+      {
+        text: "Vue 笔面试题汇总",
+        link: "/面试复盘/Vue/vue笔面试题汇总",
+      },
+    ],
+  },
 ];
